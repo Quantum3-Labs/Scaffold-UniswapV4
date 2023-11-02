519126b005f738a03688203a37608abb5d0eb4cc
karlavasquez <karlavasquez817@gmail.com>
2023-11-02 18:14:05 -0500

    feat:Improved Error Handling and Code Readability in Blockexplorer

--- packages/nextjs/pages/blockexplorer/index.tsx
+++ packages/nextjs/pages/blockexplorer/index.tsx
@@ -7,44 +7,48 @@ import { TransactionsTable } from "~~/components/blockexplorer/TransactionsTable
 import { useFetchBlocks } from "~~/hooks/scaffold-eth";
 import { getTargetNetwork, notification } from "~~/utils/scaffold-eth";
 
+const LOCALHOST_ERROR_MESSAGE = (
+  <>
+    <p className="font-bold mt-0 mb-1">Cannot connect to local provider</p>
+    <p className="m-0">
+      - Did you forget to run <code className="italic bg-base-300 text-base font-bold">yarn chain</code> ?
+    </p>
+    <p className="mt-1 break-normal">
+      - Or you can change <code className="italic bg-base-300 text-base font-bold">targetNetwork</code> in{" "}
+      <code className="italic bg-base-300 text-base font-bold">scaffold.config.ts</code>
+    </p>
+  </>
+);
+
+const TARGET_NETWORK_ERROR_MESSAGE = (
+  <>
+    <p className="font-bold mt-0 mb-1">
+      <code className="italic bg-base-300 text-base font-bold"> targeNetwork </code> is not localhost
+    </p>
+    <p className="m-0">
+      - You are on <code className="italic bg-base-300 text-base font-bold">{getTargetNetwork().name}</code> .This block
+      explorer is only for <code className="italic bg-base-300 text-base font-bold">localhost</code>.
+    </p>
+    <p className="mt-1 break-normal">
+      - You can use{" "}
+      <a className="text-accent" href={getTargetNetwork().blockExplorers?.default.url}>
+        {getTargetNetwork().blockExplorers?.default.name}
+      </a>{" "}
+      instead
+    </p>
+  </>
+);
+
 const Blockexplorer: NextPage = () => {
   const { blocks, transactionReceipts, currentPage, totalBlocks, setCurrentPage, isLoading, error } = useFetchBlocks();
 
   useEffect(() => {
     if (getTargetNetwork().id === hardhat.id && error) {
-      notification.error(
-        <>
-          <p className="font-bold mt-0 mb-1">Cannot connect to local provider</p>
-          <p className="m-0">
-            - Did you forget to run <code className="italic bg-base-300 text-base font-bold">yarn chain</code> ?
-          </p>
-          <p className="mt-1 break-normal">
-            - Or you can change <code className="italic bg-base-300 text-base font-bold">targetNetwork</code> in{" "}
-            <code className="italic bg-base-300 text-base font-bold">scaffold.config.ts</code>
-          </p>
-        </>,
-      );
+      notification.error(LOCALHOST_ERROR_MESSAGE);
     }
 
     if (getTargetNetwork().id !== hardhat.id) {
-      notification.error(
-        <>
-          <p className="font-bold mt-0 mb-1">
-            <code className="italic bg-base-300 text-base font-bold"> targeNetwork </code> is not localhost
-          </p>
-          <p className="m-0">
-            - You are on <code className="italic bg-base-300 text-base font-bold">{getTargetNetwork().name}</code> .This
-            block explorer is only for <code className="italic bg-base-300 text-base font-bold">localhost</code>.
-          </p>
-          <p className="mt-1 break-normal">
-            - You can use{" "}
-            <a className="text-accent" href={getTargetNetwork().blockExplorers?.default.url}>
-              {getTargetNetwork().blockExplorers?.default.name}
-            </a>{" "}
-            instead
-          </p>
-        </>,
-      );
+      notification.error(TARGET_NETWORK_ERROR_MESSAGE);
     }
   }, [error]);
 
